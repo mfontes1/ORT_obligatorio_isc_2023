0ba1a04cd4303f828d93cc696e2e28cfd862eeb7
mfontes1 <marcio@lacnic.net>
2023-06-20 22:49:44 -0300

    Correcion de permisos para ejecucion de docker

--- terraform/modules/dev_deploy_isc/instances.tf
+++ terraform/modules/dev_deploy_isc/instances.tf
@@ -5,15 +5,10 @@ resource "aws_instance" "module-web1-instance-deploy" {
     vpc_security_group_ids = [aws_security_group.web-SG.id]
     subnet_id              = aws_subnet.pub_subnet1_obligatorio.id
     availability_zone      = var.vpc_aws_az
-    user_data     = <<-EOF
+    user_data = <<-EOF
     #!/bin/bash
-    yum update -y
-    yum install -y git curl docker 
-    sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
-    sudo usermod -aG docker ec2-user"
-    
-  EOF      
+    sudo yum update -y        
+EOF
     connection {
       type = "ssh"
       user = "ec2-user"
@@ -21,45 +16,19 @@ resource "aws_instance" "module-web1-instance-deploy" {
       host = self.public_ip
     }
     provisioner "remote-exec" {
-    inline = [
+      inline = [
         
-        #"sudo yum update",
-        #"sudo yum install -y nginx git curl",
-        #"sudo git clone https://github.com/mauricioamendola/chaos-monkey-app.git /tmp/chaos-monkey-app",
-        #"sudo mv /usr/share/nginx/html /usr/share/nginx/html_backup",
-        #"sudo cp -r /tmp/chaos-monkey-app /usr/share/nginx/html",
-        #"sudo chown -R nginx:nginx /usr/share/nginx/html",    
-        #"sudo chmod -R 755 /usr/share/nginx/html",
-        #"sudo sed -i 's#root /usr/share/nginx/html;#root /usr/share/nginx/html; index index.html;#' /etc/nginx/nginx.conf",       
-        #"sudo systemctl enable nginx",
-        #"sudo systemctl start nginx",
-        #"sudo yum install -y git docker curl",
-        #"git clone https://github.com/mfontes1/ORT_obligatorio_isc_2023.git",
-        #"sudo mv ORT_obligatorio_isc_2023/docs/img/maxresdefault.jpg /var/www/html/",
-        #"sudo usermod -aG docker ec2-user",
-        #"sudo amazon-linux-extras install -y nginx1",
-        #"sudo yum update",
-        #"cd ORT_obligatorio_isc_2023/terraform/modules/dev_deploy_isc/docker-compose",
-        #"sudo yum update -y",
-        #"sudo yum install -y docker",        
-        #"mkdir /home/ec2-user/proyecto_isc",        
-        #"sudo pip3 install docker-compose",
-   
-        "exit",
-        "mkdir -p /home/ec2-user/proyecto",
-        "cd /home/ec2-user/proyecto",
-        "git clone https://github.com/mfontes1/ORT_obligatorio_isc_2023.git ",               
-        "cd ORT_obligatorio_isc_2023/terraform/modules/dev_deploy_isc/docker-compose/",
+        "sudo yum install -y git curl docker",
+        "sudo curl -L \"https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose",
+        "sudo chmod +x /usr/local/bin/docker-compose",
+        "sudo usermod -aG docker ec2-user",
+        "cd /home/ec2-user/",
+        "sudo git clone https://github.com/mfontes1/ORT_obligatorio_isc_2023.git",      
         "sudo systemctl enable docker",
         "sudo systemctl start docker",
-        "docker-compose up -d",           
-        #"git clone https://github.com/mfontes1/ORT_obligatorio_isc_2023.git", 
-        #"sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/sbin/docker-compose",
-        #"sudo chmod +x /usr/local/sbin/docker-compose",
-
-        #"cd ORT_obligatorio_isc_2023/terraform/modules/dev_deploy_isc/docker-compose",
-        #"sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/sbin/docker-compose",
-        #"sudo chmod +x /usr/local/sbin/docker-compose",
+        "cd /home/ec2-user/ORT_obligatorio_isc_2023/terraform/modules/dev_deploy_isc/docker-compose/",      
+        "docker-compose up -d"
+        
                 
   ]
 }
@@ -77,8 +46,8 @@ resource "aws_instance" "module-web2-instance-deploy" {
     availability_zone      = var.vpc_aws_az2
     user_data     = <<-EOF
     #!/bin/bash
-    yum update -y
-    yum install -y git curl docker
+    sudo yum update -y
+    
      
   EOF
     connection {
@@ -88,17 +57,18 @@ resource "aws_instance" "module-web2-instance-deploy" {
       host = self.public_ip
     }
     provisioner "remote-exec" {
-      inline = [ 
-       
-        #"sudo pip3 install docker-compose",        
+      inline = [
+        "sudo yum install -y git curl docker",
+        "sudo curl -L \"https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose",
+        "sudo chmod +x /usr/local/bin/docker-compose",
+        "sudo usermod -aG docker ec2-user",
+        "cd /home/ec2-user/",
+        "sudo git clone https://github.com/mfontes1/ORT_obligatorio_isc_2023.git",        
         "sudo systemctl enable docker",
-        "sudo systemctl start docker",
-        "mkdir proyecto_isc",
-        #"cd proyecto_isc  | sudo git clone https://github.com/mfontes1/ORT_obligatorio_isc_2023.git",  
-        #"cd ORT_obligatorio_isc_2023/terraform/modules/dev_deploy_isc/docker-compose",
-        #"sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/sbin/docker-compose",
-        #"sudo chmod +x /usr/local/sbin/docker-compose",
-        
+        "sudo systemctl start docker", 
+        #"cd home/ec2-user/ORT_obligatorio_isc_2023/terraform/modules/dev_deploy_isc/docker-compose/",     
+        #"docker-compose up -d"
+                
        ]
     }    
     tags = {
@@ -115,9 +85,8 @@ resource "aws_instance" "module-cache-instance-deploy" {
     availability_zone      = var.vpc_aws_az
     user_data     = <<-EOF
     #!/bin/bash
-    yum update -y
-    yum install -y git curl docker
-       
+    sudo yum update -y
+           
   EOF
 
     connection {
@@ -127,32 +96,18 @@ resource "aws_instance" "module-cache-instance-deploy" {
       host = self.public_ip
     }
     provisioner "remote-exec" {
-    inline = [
-        
-        #"sudo yum update",
-        #"sudo amazon-linux-extras install -y nginx1",
-        #"sudo yum install -y  git curl",
-        #"sudo git clone https://github.com/mauricioamendola/chaos-monkey-app.git /tmp/chaos-monkey-app",
-        #"sudo mv /usr/share/nginx/html /usr/share/nginx/html_backup",
-        #"sudo cp -r /tmp/chaos-monkey-app /usr/share/nginx/html",
-        #"sudo chown -R nginx:nginx /usr/share/nginx/html",    
-        #"sudo chmod -R 755 /usr/share/nginx/html",
-        #"sudo sed -i 's#root /usr/share/nginx/html;#root /usr/share/nginx/html; index index.html;#' /etc/nginx/nginx.conf",       
-        #"sudo systemctl enable nginx",
-        #"sudo systemctl start nginx",
-        #"sudo yum install -y git docker-ce",
-        #"git clone https://github.com/mfontes1/ORT_obligatorio_isc_2023.git",
-        #"sudo yum update -y",        
-        #"sudo pip3 install docker-compose",        
+    inline = [        
+       "sudo yum install -y git curl docker",
+        "sudo curl -L \"https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose",
+        "sudo chmod +x /usr/local/bin/docker-compose",
+        "sudo usermod -aG docker ec2-user",
+        "cd /home/ec2-user/",
+        "sudo git clone https://github.com/mfontes1/ORT_obligatorio_isc_2023.git",
         "sudo systemctl enable docker",
         "sudo systemctl start docker",
-        "mkdir /home/ec2-user/proyecto_isc/",
-        #"cd /home/ec2-user/proyecto_isc/ ", 
-        #"git clone https: //github.com/mfontes1/ORT_obligatorio_isc_2023.git",       
-        #"cd ORT_obligatorio_isc_2023/terraform/modules/dev_deploy_isc/docker-compose",
-        #"sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/sbin/docker-compose",
-        #"sudo chmod +x /usr/local/sbin/docker-compose",
-        #"docker-compose -h",
+        #"cd ORT_obligatorio_isc_2023/terraform/modules/dev_deploy_isc/docker-compose/",      
+        #"docker-compose up -d" 
+              
   ]
 }
 tags = {
