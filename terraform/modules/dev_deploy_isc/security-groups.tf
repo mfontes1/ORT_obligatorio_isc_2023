d23713e88d2a792763d1c15cfa4e4c5ada70f21d
mfontes1 <marcio@lacnic.net>
2023-07-05 13:06:02 -0300

    se retiran algunas variables en shipping-service

--- terraform/modules/dev_deploy_isc/security-groups.tf
+++ terraform/modules/dev_deploy_isc/security-groups.tf
@@ -37,13 +37,13 @@ resource "aws_security_group" "web_lb_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 0
